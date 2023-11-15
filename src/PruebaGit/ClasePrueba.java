f8f43e1d5d7ecd366a784ef503055f045adaddfe
IgancioCab <cignacio942@gmail.com>
2023-11-15 16:02:38 -0300

    modelo base de datos

---
+++ src/PruebaGit/ClasePrueba.java
@@ -0,0 +1,9 @@
+package PruebaGit;
+
+public class ClasePrueba {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hice esta clase para practicar la coneccion del programa con git");
+    }
+}
