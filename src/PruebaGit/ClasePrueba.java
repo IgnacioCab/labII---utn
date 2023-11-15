97a373f2dc1b4036ca63375b7ebe1cf6447a540a
IgancioCab <cignacio942@gmail.com>
2023-11-15 17:22:05 -0300

    Prueba de git

--- src/PruebaGit/ClasePrueba.java
+++
@@ -1,9 +0,0 @@
-package PruebaGit;
-
-public class ClasePrueba {
-
-    public static void main(String[] args) {
-
-        System.out.println("Hice esta clase para practicar la coneccion del programa con git");
-    }
-}
